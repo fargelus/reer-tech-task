64e8ebc8ea679e780b1c224daf7e3a4c39ba1f3b
fargelus <ddraudred@gmail.com>
2020-02-10 12:18:29 +0300

    Short url label at show page + styles structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
   def short_url(shorten)
     "#{request.protocol}#{request.host_with_port}/#{shorten}"
   end
+
+  def funny_enter_url_phrase
+    phrases = [
+      'One URL shortener to rule them all...',
+      'Try it again if you brave enough:',
+      'One bear, two bear, the game is over, another url please:',
+      'Once again'
+    ]
+    total = phrases.size
+    phrases[rand(total)]
+  end
 end
